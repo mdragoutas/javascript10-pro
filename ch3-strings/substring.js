ec3ce4ae5ed0e4b6d885015921340f832011088e
mdragoutas <mdragoutas@gmail.com>
2026-07-09 19:21:26 +0300

    feat: add string manipulation examples for substring and traversal

---
+++ ch3-strings/substring.js
@@ -0,0 +1,10 @@
+let str = 'Coding Factory';
+
+console.log(str.substring(1 , 5));
+console.log(str.slice(1 , 5));
+console.log(str.slice(2));
+console.log(str.slice(-7));
+
+// split
+let tokens = str.split(' ');
+console.log(tokens)
