87ce48834de33da53ddbd9c074fe8963a59a5a77
mdragoutas <mdragoutas@gmail.com>
2026-07-09 19:40:22 +0300

    feat: add examples for string concatenation and search methods

---
+++ ch3-strings/search.js
@@ -0,0 +1,10 @@
+let str = 'Coding Factory';
+
+// charAt(i)
+console.log(str.charAt(1));         // 'o'
+console.log(str.charAt(100));       // ' '
+console.log(str[100]);              // 'undefined'
+
+//indexOf('x')
+console.log(str.indexOf('o'));       // 1
+console.log(str.lastIndexOf('o'));   // 11   
